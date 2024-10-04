198c7a3b918adf923201c59f5aa448cb82faf4ee
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2024-10-04 12:52:30 -0400

    Create hook for carousel timer

---
+++ src/hooks/useCarouselTimer.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export const useCarouselTimer = (
+  isPlaying: boolean,
+  update: () => void,
+  intervalInMillis: number
+) => {
+  useEffect(() => {
+    let interval: number | undefined = undefined;
+
+    if (isPlaying) interval = setInterval(update, intervalInMillis);
+    else clearInterval(interval);
+
+    return () => clearInterval(interval);
+  });
+};
