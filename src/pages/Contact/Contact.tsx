39e8615ba1993931bf8af3c7171f33bd65237159
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2024-10-17 18:16:56 -0400

    Add Contact page

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,23 @@
+import { Container, Typography } from "@mui/material";
+import { NavBar } from "../../components/NavBar";
+
+export const Contact = () => {
+  return (
+    <>
+      <head>
+        <title>JJ : Contact</title>
+      </head>
+      <body>
+        <NavBar />
+        <main>
+          <Container>
+            <Typography variant="h3" marginTop={4} marginBottom={2}>
+              Contact
+            </Typography>
+            <Typography variant="h5">Coming soon!</Typography>
+          </Container>
+        </main>
+      </body>
+    </>
+  );
+};
